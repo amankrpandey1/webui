c883d312ba29fe3faf2dd1d5953710923b1aa00a
Aman Pandey <lazzzy@Amans-MacBook-Air.local>
2022-11-04 20:14:14 +0530

    all changes done for desktop settings and phone 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-$("video").prop('muted', true);
+$(".banner-vid").prop('muted', true);
 
 // $(document).ready(function() {
 // $("#switch-container").click(function (){
@@ -10,6 +10,15 @@ $("video").prop('muted', true);
 //   });
 // });
 
+document.querySelectorAll('a[href^="#"]').forEach($anchor => {
+    $anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start' //scroll to top of the target element
+        });
+    });
+});
 
 $(document).ready(function() {
 
@@ -59,10 +68,10 @@ function toggleSoundOnOff()  {
     soundOn = true
     console.log("soundOn: ", soundOn)
   }
-  if( $("video").prop('muted') ) {
-    $("video").prop('muted', false);
+  if( $(".banner-vid").prop('muted') ) {
+    $(".banner-vid").prop('muted', false);
   } else {
-    $("video").prop('muted', true);
+    $(".banner-vid").prop('muted', true);
   }
 }
 
@@ -76,3 +85,87 @@ function toggleSoundOnOff()  {
       sound.play();      
     }
   }
+
+  /*
+Class for scrolling effect: ons croll moves to the next section, appending classes to the section: current, prev and next, and making prev and next sections darker.
+*/
+
+const myPage = {
+    // Scroll to the block
+    scrollThere: function(targetElement, speed) {
+      var _self = this;
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: targetElement.offset().top
+          },
+          {
+            duration: speed,
+            complete: function() {
+              _self.setViewportClasses(); // On complete set viewport classes
+            }
+          }
+        );
+    },
+  
+    //Check if element is in the viewport
+    isInViewport: function(element) {
+      var _self = this;
+      // check for the section which is the main currently
+      var elementMiddle = element.offset().top + element.outerHeight() / 2;
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+      return elementMiddle > viewportTop && elementMiddle < viewportBottom;
+    },
+  
+    // Set classes for the current element, prev and next
+    setViewportClasses: function() {
+      var _self = this;
+      // add classes for main, prev and next sections
+      $(".inView").removeClass("inView");
+      $(".prev-inView").removeClass("prev-inView");
+      $(".next-inView").removeClass("next-inView");
+      $("section").each(function(i, obj) {
+        if (_self.isInViewport($(this))) {
+          $(this).addClass("inView");
+          if ($(this).prev())
+            $(this)
+              .prev()
+              .addClass("prev-inView");
+          if ($(this).next())
+            $(this)
+              .next()
+              .addClass("next-inView");
+          return false;
+        }
+      });
+    },
+  
+    initEvents: function() {
+      var _self = this;
+      $(window)
+        .off()
+        .on("wheel", function(e) {
+          e.preventDefault;
+          if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+            //scroll up
+            if ($(".prev-inView").length > 0)
+              _self.scrollThere($(".prev-inView"), 500);
+          } else {
+            //scroll down
+            if ($(".next-inView").length > 0)
+              _self.scrollThere($(".next-inView"), 500);
+          }
+        });
+    },
+  
+    init: function() {
+      var _self = this;
+      _self.setViewportClasses();
+      _self.initEvents();
+    }
+  };
+  
+  myPage.init();
+  
